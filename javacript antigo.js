f509007864c3c4c7122817444b7c64e8ef0866c0
Ana <anacabh@gmail.com>
2023-11-24 11:40:56 -0300

    files

---
+++ javacript antigo.js
@@ -0,0 +1,43 @@
+  #slide2{
+    z-index: 2;
+    display: none;
+  }
+
+  window.addEventListener("scroll", function() {
+    let scrollPosition = window.scrollY;
+    let parallaxContainer = document.getElementById("parallax-container");
+    let slideHeight = window.innerHeight;
+    let currentSlide = Math.floor(scrollPosition / slideHeight) + 1;
+    parallaxContainer.style.backgroundColor = getSlideColor(currentSlide);
+  });
+
+  function getSlideColor(slideNumber) {
+    switch (slideNumber) {
+      case 1:
+        return "var(--verde)";
+      case 2:
+        return "var(--laranja)";
+      case 3:
+        return "var(--roxo)";
+      case 4:
+        return "var(--azul)";
+      case 5:
+        return "var(--verde-escuro)";
+      case 6:
+        return "var(--verde)";
+      case 7:
+        return "var(--laranja)";
+      case 8:
+        return "var(--roxo)";
+      case 9:
+        return "var(--azul";
+      case 10:
+        return "var(--verde-escuro";
+      case 11:
+        return "var(--verde)";
+      case 12:
+        return "var(--laranja)";
+      default:
+        return "var(--verde)";
+    }
+  }
